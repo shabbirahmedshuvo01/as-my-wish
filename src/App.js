7acc9cc31260adc20f7b45935c2bef14d96a3aba
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-06-22 02:52:29 +0600

    here navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       {/* <h2 className='position' style={{ paddingLeft: '75px', border: '5px solid red', marginLeft: "550px", marginRight: "500px", marginTop: "560px" }} >Here As my wish</h2> */}
 
 
+      <Navbar></Navbar>
       <h2 className='position' style={{ textAlign: "center" }}>Here As my wish</h2>
     </div >
   );
